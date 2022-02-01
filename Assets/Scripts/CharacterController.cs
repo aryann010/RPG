9675ce30250074ed1ba808a905064fb7fea5ad30
Aryan <aryannaidu010@gmail.com>
2022-02-01 14:06:19 +0530

    player movement based on user's input (basic)

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CharacterController : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    protected Vector2 direction;
+
+    protected virtual void Update()
+    {
+        move();
+    }
+
+    private void move()
+    {
+        transform.Translate( direction * speed * Time.deltaTime);
+        
+    }
+}
